3430e474f8ff3303dfb0257abc2718ecaeb36e9e
hassanradi93 <hassan93radi@gmail.com>
2019-05-03 19:43:31 +0300

    my first comming -admin vews created

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hasan
+ * Date: 4/28/2019
+ * Time: 3:53 PM
+ */
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin');
+
+@section('content')
+
+    <h1>Users</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Active</th>
+            <th>Created</th>
+            <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active==1 ? 'Active':'Not Active'}}</td>{{-- is the user is active by3rod active eza la2 ma by3rod--}}
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+       </tbody>
+    </table>
+@stop
